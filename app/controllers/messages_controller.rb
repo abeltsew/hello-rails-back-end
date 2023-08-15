c7b66bc2c2ef1d902e830854e7878b74a46ff3d4
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-08-15 15:30:51 +0300

    linter fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
-    def index
-        @random_message = Message.find(Message.pluck(:id).sample)
-        render json: @random_message
-    end
+  def index
+    @random_message = Message.find(Message.pluck(:id).sample)
+    render json: @random_message
+  end
 end
